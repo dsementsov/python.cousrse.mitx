3ac79ce3993d5a2fe323c65927b6dd6d98a76e33
thebarsik <thebarsik@gmail.com>
2018-06-29 13:42:24 +0200

    Extencion change

---
+++ week2_Problem1.py
@@ -0,0 +1,23 @@
+# guess my number 
+
+
+def guess_my_number ():
+    guess_low = 0
+    guess_high = 100
+    print ("Please think of a number between " + str(guess_low) +  " and " + str(guess_high) + "!")
+    while True:
+        guess = int((guess_high + guess_low)/2)
+        print("Is your secret number " + str(guess) + "?")
+        print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+        guess_direction = input()
+        if guess_direction == "h":
+            guess_high = guess - 1
+        elif guess_direction == "l":
+            guess_low = guess + 1
+        elif guess_direction == "c":
+            print("Game over. Your secret number was: " + str(guess))
+            break
+        else:
+            print("Sorry, I did not understand your input.")
+
+guess_my_number()
