be05085463b3515793da85d2e2786818c7dca40d
thebarsik <thebarsik@gmail.com>
2018-06-29 14:27:24 +0200

    Refactoring

--- week2_GuessMyNumber.py
+++ week2_GuessMyNumber.py
@@ -8,16 +8,14 @@ def guess_my_number ():
     while True:
         guess = int((guess_high + guess_low)/2)
         print("Is your secret number " + str(guess) + "?")
-        print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
-        guess_direction = input()
+        guess_direction = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")   
         if guess_direction == "h":
-            guess_high = guess - 1
+            guess_high = guess
         elif guess_direction == "l":
-            guess_low = guess + 1
+            guess_low = guess
         elif guess_direction == "c":
             print("Game over. Your secret number was: " + str(guess))
             break
         else:
             print("Sorry, I did not understand your input.")
-
 guess_my_number()
