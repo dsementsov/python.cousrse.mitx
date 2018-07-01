69c56514dabeef6bc9ff5b9bdd36122eb0a46077
thebarsik <thebarsik@gmail.com>
2018-07-01 12:30:28 +0200

    week 2 problem set revised

--- week2_problem3.py
+++ week2_problem3.py
@@ -17,16 +17,23 @@ def minimal_payment_bisection ( balance, annual_interest_rate, epsilon = 0.01):
     # monthly payment would be 1/12 of the balance, 
     # after having its interest compounded monthly for an entire year.
     higher_bound = week2_problem2.after_year_balance_recoursive(balance, annual_interest_rate, 0) / 12 
-
+    
+    # initial balance
     new_balance = balance
+    # initial guess
     payment_guess = 0
 
+    # main bisection loop conditions: balance payed, payed by epsilon marigin
     while new_balance > 0 or abs(new_balance) > epsilon:
+        # moving lower bound if the guess is too low
         if new_balance > 0:
             lower_bound = payment_guess
+        # moving upper bound if the guess is too high
         elif new_balance < 0:
             higher_bound = payment_guess
+        #calculating new guess and balance with new guess
         payment_guess = (higher_bound + lower_bound) / 2
         new_balance = week2_problem2.after_year_balance_recoursive(balance, annual_interest_rate, payment_guess)
+    # return result rounded by 2 decimals
     return round(payment_guess, 2)
 print ("Lowest payment:", minimal_payment_bisection(balance, annualInterestRate))
